6c8e1e0a11e1d169d077b03bf5bee6598213ae7d
원치현 <heungeob1003@gmail.com>
2021-10-20 15:58:31 +0900

    Import BrowserRouter and Wrap App Component by BrowserRouter

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
 function Home() {
   return ( 
@@ -40,6 +41,7 @@ function App() {
     </div>
   );
 }
+ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
