420b4a96cdca6fcd69f7e90e7ce903d26ec880c0
원치현 <heungeob1003@gmail.com>
2021-10-20 15:54:36 +0900

    Install react-router-dom

--- src/index.js
+++ src/index.js
@@ -40,7 +40,6 @@ function App() {
     </div>
   );
 }
-ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
