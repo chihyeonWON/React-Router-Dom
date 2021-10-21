31296bffa4f39e6f1e3e534ba0a7061d22c81116
원치현 <heungeob1003@gmail.com>
2021-10-21 15:32:54 +0900

    Change BrowserRouter to HashRouter that means bookmark

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+import { HashRouter, Route, Switch, Link } from 'react-router-dom';
 
 function Home() {
   return ( 
@@ -49,7 +49,7 @@ function App() {
     </div>
   );
 }
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
+ReactDOM.render(<HashRouter><App /></HashRouter>, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
